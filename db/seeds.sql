c087bb0739cc57f08d719e827781cdffd1065c37
Mets0811 <melissa0811@icloud.com>
2022-03-20 19:36:38 -0400

    added salaries

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,29 +12,29 @@ INSERT INTO department (name)
 VALUES ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 60000, 1);
+VALUES ("Sales Lead", 97000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
+VALUES ("Lead Engineer", 160000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
+VALUES ("Software Engineer", 115000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 120000, 3);
+VALUES ("Accountant", 100000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 100000, 4);
+VALUES ("Legal Team Lead", 75000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Juan", "DelPueblo", 1, 3);
+VALUES ("Melissa", "Moreno", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Da", "Chan", 2, 1);
+VALUES ("John", "Klein", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Susana", "Rodriguez", 3, null);
+VALUES ("Matt", "Baldoni", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Bad", "Bunny", 4, 3);
+VALUES ("Crystal", "Wells", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Maria", "DelCarmen", 5, null);
+VALUES ("Maria", "Santos", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jennifer", "Lopez", 2, null);
+VALUES ("Chris", "Lopez", 2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Hanks", 4, 7);
+VALUES ("Tom", "Hill", 4, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Christian", "Castro", 1, 2);
+VALUES ("Javante", "Smith", 1, 2);
