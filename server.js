1e8f53004b379ade2171c87ca7251ef31d22cb0d
Mets0811 <melissa0811@icloud.com>
2022-03-20 19:50:54 -0400

    env

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
 require('dotenv').config();
-// const sql = require("./db"); --maybe??????????
-// console.log(process.env)
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
-  database: "employeesDB"
+  database: "employees_DB"
 });
 
 
